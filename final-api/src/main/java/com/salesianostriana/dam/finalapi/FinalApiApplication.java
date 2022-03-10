f552a4aa76153e426d4d7f5c941b75afa5dbdea1
efatuarte <fatuarte.feern21@triana.salesianos.edu>
2022-03-10 22:04:24 +0100

    Set up API

---
+++ final-api/src/main/java/com/salesianostriana/dam/finalapi/FinalApiApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.finalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalApiApplication.class, args);
+	}
+
+}
