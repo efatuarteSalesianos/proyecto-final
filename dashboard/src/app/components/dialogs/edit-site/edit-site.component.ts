af1fdd9ce8c25783ec1a257ca28004ea8ec264df
efatuarte <fatuarte.feern21@triana.salesianos.edu>
2022-06-13 14:17:22 +0200

    Edit site and user form styles

--- dashboard/src/app/components/dialogs/edit-site/edit-site.component.ts
+++ dashboard/src/app/components/dialogs/edit-site/edit-site.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { EditSiteDTO } from 'src/app/models/dto/edit-site.dto';
+import { SiteDTO } from 'src/app/models/dto/site.dto';
 import { SiteResponse } from 'src/app/models/interfaces/site.interface';
 import { PropietarioResponse } from 'src/app/models/interfaces/user.interface';
 import { SiteService } from 'src/app/services/site.service';
+import { UploadFileService } from 'src/app/services/upload-file.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -13,23 +16,46 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class EditSiteComponent implements OnInit {
 
-  siteDto = new EditSiteDTO();
+  editSiteForm!: FormGroup;
+  siteDto = new SiteDTO();
   site!: SiteResponse;
   propietariosList: PropietarioResponse [] = [];
-  constructor(private siteService: SiteService, private snackBar: MatSnackBar, private userService: UserService) { }
+  loading: boolean = false; // Flag variable
+  fileName="";
+  file: File | null = null;
+  constructor(private fileUploadService: UploadFileService, private siteService: SiteService, private snackBar: MatSnackBar, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.editSiteForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      postalCode: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      web: new FormControl('', [Validators.required]),
+      propietarioId: new FormControl('', [Validators.required]),
+    });
     this.userService.listarPropietarios().subscribe(result => {
       this.propietariosList = result;
     });
   }
 
-  addSite() {
+  editSite() {
     if(this.siteDto.name===""||this.siteDto.address===""||
       this.siteDto.city===""||this.siteDto.email===""||this.siteDto.postalCode===""
       ||this.siteDto.phone===""){
       this.snackBar.open('Faltan datos del negocio', 'Aceptar');
     }else{
+      this.siteDto.name = this.editSiteForm.get('name')?.value;
+      this.siteDto.address = this.editSiteForm.get('address')?.value;
+      this.siteDto.city = this.editSiteForm.get('city')?.value;
+      this.siteDto.email = this.editSiteForm.get('email')?.value;
+      this.siteDto.postalCode = this.editSiteForm.get('postalCode')?.value;
+      this.siteDto.phone = this.editSiteForm.get('phone')?.value;
+      this.siteDto.web = this.editSiteForm.get('web')?.value;
+      this.siteDto.propietarioId = this.editSiteForm.get('propietarioId')?.value;
       this.siteService.addSite(this.siteDto).subscribe(result => {
         this.site = result;
         this.snackBar.open('Se ha creado el negocio correctamente', 'Aceptar');
@@ -38,4 +64,11 @@ export class EditSiteComponent implements OnInit {
     }
   }
 
+  handleFileInput(files: FileList) {
+    this.file = files.item(0);
+    this.fileName = this.file!.name;
+    this.fileUploadService.upload(this.file!).subscribe(result => {
+      console.log(result);
+    });
+  }
 }
--- dashboard/src/app/components/dialogs/edit-user/edit-user.component.ts
+++ dashboard/src/app/components/dialogs/edit-user/edit-user.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { EditUserDTO } from 'src/app/models/dto/editUser.dto';
+import { RegisterDTO } from 'src/app/models/dto/register.dto';
 import { UserResponse } from 'src/app/models/interfaces/user.interface';
+import { UploadFileService } from 'src/app/services/upload-file.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -11,24 +14,50 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class EditUserComponent implements OnInit {
 
-  userDto = new EditUserDTO();
+  editUserForm!: FormGroup;
+  userDto = new RegisterDTO();
   user!: UserResponse;
-  constructor(private snackBar: MatSnackBar, private userService: UserService) { }
+  loading: boolean = false; // Flag variable
+  fileName="";
+  file: File | null = null;
+
+  constructor(private fileUploadService: UploadFileService, private snackBar: MatSnackBar, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.editUserForm = new FormGroup({
+      fullName: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      birthDate: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+    })
   }
 
-  editUser() {
+  addUser() {
     if(this.userDto.fullName===""||this.userDto.username===""||
       this.userDto.email===""||this.userDto.email===""||this.userDto.phone==="") {
       this.snackBar.open('Faltan datos del usuario', 'Aceptar');
     }else{
+      this.userDto.fullName = this.editUserForm.get('fullName')?.value;
+      this.userDto.username = this.editUserForm.get('username')?.value;
+      this.userDto.email = this.editUserForm.get('email')?.value;
+      this.userDto.birthDate = this.editUserForm.get('birthDate')?.value;
+      this.userDto.phone = this.editUserForm.get('phone')?.value;
+      this.userDto.password = this.editUserForm.get('password')?.value;
       this.userService.addUser(this.userDto).subscribe(result => {
         this.user = result;
-        this.snackBar.open('Se ha editado el usuario correctamente', 'Aceptar');
+        this.snackBar.open('Se ha creado el usuario correctamente', 'Aceptar');
         history.go(0)
       });
     }
   }
 
+  handleFileInput(files: FileList) {
+    this.file = files.item(0);
+    this.fileName = this.file!.name;
+    this.fileUploadService.upload(this.file!).subscribe(result => {
+      console.log(result);
+    });
+  }
 }
